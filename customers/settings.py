cd05a1fae550ab1982974b2423db97c82a453656
biojoel21 <eebc_5@hotmail.com>
2025-04-23 14:10:13 -0400

    terminar capt 48

--- customers/settings.py
+++ customers/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+from datetime import timedelta
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -27,12 +27,20 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=15),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+    "ROTATE_REFRESH_TOKENS": True, 
+    "BLACKLIST_AFTER_ROTATION": False,    
+    "REFRESH_TOKEN_INACTIVITY_LIFETIME": timedelta(minutes=15),
+}
 
 # Application definition
 
 INSTALLED_APPS = [
     'corsheaders',
     'rest_framework',
+    'rest_framework_simplejwt.token_blacklist',
     'customers',
     'django.contrib.admin',
     'django.contrib.auth',
